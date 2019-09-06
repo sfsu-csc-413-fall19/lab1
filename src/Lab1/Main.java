18012df719c2020b8160c5c5e5a7d553a6ec63ac
brianxautumn <shred@brianautumn.com>
2019-09-05 20:39:00 -0700

    Classwork

--- src/Lab1/Main.java
+++ src/Lab1/Main.java
@@ -61,7 +61,7 @@ public class Main {
         writer.println("");
 
         // Body of our response
-        writer.println("<h1>Some cool response!</h1>");
+        writer.println("<h1>Some cooler response!</h1>");
 
         dong.close();
       }
